774f674d5c4a8b7972cbd35c89f7eec236c0e757
Saleh Saeed <saleh.saiid@gmail.com>
2016-06-12 15:47:48 +0300

    Add User model

--- server/Models/User.js
+++ server/Models/User.js
@@ -1,3 +1,23 @@
 /**
  * Created by saleh on 6/12/16.
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    first_name: { type: String },
+    last_name: { type: String },
+    email: { type: String },
+    country_code: { type: String },
+    phone_number: { type: String },
+    last_location: { type: String },
+    friends: { type: String },
+    devices: { type: String },
+    is_active: { type: Boolean },
+    confirm_code: { type: String },
+    is_confirmed: { type: Boolean },
+    created_date: { type: Date }
+});
+
+
+mongoose.model('User', User);
