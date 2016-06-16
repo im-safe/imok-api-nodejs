5a333f6cf1a69f33a44f268e138802f2a614705e
Saleh Saeed <saleh.saiid@gmail.com>
2016-06-16 22:52:51 +0300

    Response user info

--- server/Controllers/Users.js
+++ server/Controllers/Users.js
@@ -1,13 +1,14 @@
 /**
  * Created by saleh on 6/12/16.
  */
-var User = require('../Models/User');
+var User = require('../Models/User').model;
+var DevicesService = require('../Services/DevicesService');
 
 exports.create = function(req, res) {
     // Validate Data
     req.checkBody({
-        country_code: { notEmpty: true},
-        phone_number: { notEmpty: true}
+        country_code: { notEmpty: true },
+        phone_number: { notEmpty: true }
     });
 
     var errors = req.validationErrors();
@@ -24,12 +25,13 @@ exports.create = function(req, res) {
 
     var userData = {
         country_code: req.body.country_code,
-        phone_number: req.body.phone_number
+        phone_number: req.body.phone_number,
+        is_active: true
     };
 
     var user = new User(userData);
 
-    user.save(function(err){
+    user.save(function(err, user){
         if(err){
             return res.status(400).json({
                 error: true,
@@ -38,9 +40,19 @@ exports.create = function(req, res) {
             });
         }
 
+        var result = {
+            id : user._id,
+            friends: user.friends,
+            last_location: user.last_location,
+            country_code: user.country_code,
+            phone_number: user.phone_number,
+            device: user.device,
+            friends: user.friends
+        };
+
         return res.json({
             error: false,
-            results: user,
+            results: result,
             errors: []
         });
     });
