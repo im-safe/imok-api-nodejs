259f9e7b3355d8903be53d1b82f3e759bd47e18a
Saleh Saeed <saleh.saiid@gmail.com>
2016-06-18 13:51:11 +0300

    Implement createUser method

--- server/Services/UsersService.js
+++ server/Services/UsersService.js
@@ -4,6 +4,13 @@
 var User = require('../Models/User').model;
 var Q = require('q');
 
+/**
+ * Check user if exists
+ *
+ * @param country_code
+ * @param phone_number
+ * @returns Promise
+ */
 function checkExists(country_code, phone_number)
 {
     var deferred = Q.defer();
@@ -25,6 +32,37 @@ function checkExists(country_code, phone_number)
     return deferred.promise;
 }
 
+/**
+ * Create new user
+ *
+ * @param userData
+ * @param callback
+ */
+function createUser(userData, callback)
+{
+    var user = new User(userData);
+
+    user.save(function(err, user){
+        if(err){
+            callback(true, err);
+        }
+
+        var result = {
+            id : user._id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            country_code: user.country_code,
+            phone_number: user.phone_number,
+            friends: user.friends,
+            last_location: user.last_location,
+            device: user.device
+        };
+
+        callback(false, result);
+    });
+}
+
 module.exports = {
-    checkExists: checkExists
+    checkExists: checkExists,
+    createUser: createUser
 };
