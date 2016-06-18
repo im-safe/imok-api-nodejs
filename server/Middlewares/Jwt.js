1e23af3fe62e4a141ad65a98ac5b6fe22b12a99c
Saleh Saeed <saleh.saiid@gmail.com>
2016-06-19 01:57:25 +0300

    Implement JWT token verifier

--- server/Middlewares/Jwt.js
+++ server/Middlewares/Jwt.js
@@ -1,7 +1,34 @@
 'use strict';
+var njwt = require('njwt');
+var fs = require('fs');
 
 function authorize(req, res, next)
 {
+    var token;
+    var header = req.headers['authorization'];
+    if (typeof header !== 'undefined') {
+        var bearer = header.split(" ");
+        token = bearer[1];
+        req.token = token;
+
+        try{
+            var verifiedToken = njwt.verify(token, readSecreteKeyFile(), 'HS256');
+
+            // Set token in request
+            req.token = verifiedToken.body;
+
+            next();
+        }catch(e){
+            res.jsonError('Not Authorized', 400);
+        }
+    }else{
+        res.jsonError('Not Authorized', 400);
+    }
+}
+
+function readSecreteKeyFile()
+{
+        return fs.readFileSync(process.cwd() + '/server/Keys/imok.pem');
 }
 
 module.exports = authorize;
