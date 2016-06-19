4826e0eb9e3c2afd7f8170ba93c0d50cbc29315a
Saleh Saeed <saleh.saiid@gmail.com>
2016-06-19 22:28:04 +0300

    Add EventLog Schema

---
+++ server/Models/EventLog.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var EventLog = new Schema({
+    user_id: { type: mongoose.Types.ObjectId, index: true},
+    log_type: { type: String, index: true },
+    created_date: { type: Date, default: Date.now }
+});
+
+module.exports = {
+    schema: EventLog,
+    model: mongoose.model('EventLog', EventLog)
+};
