2c5eb2841368c8a99370a13fa3afae120517760f
Saleh Saeed <saleh.saiid@gmail.com>
2016-06-19 03:08:47 +0300

    Edit returned expires_in value

--- server/Middlewares/Jwt.js
+++ server/Middlewares/Jwt.js
@@ -2,8 +2,16 @@
 var njwt = require('njwt');
 var fs = require('fs');
 
+/**
+ * Check access token if valid
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
 function authorize(req, res, next)
 {
+    generateToken('324567');
     var token;
     var header = req.headers['authorization'];
     if (typeof header !== 'undefined') {
@@ -26,9 +34,42 @@ function authorize(req, res, next)
     }
 }
 
+/**
+ * Generate new access token
+ *
+ * @param userId
+ * @returns {{access_token: *, token_type: string, expires_in: number}}
+ */
+function generateToken(userId)
+{
+    var claims = {
+        iss: "http://imok.com/",  // Issuer
+        sub: userId,    // User ID
+        scope: "api"
+    };
+
+    var expiration_date = new Date('2030-01-01');
+
+    var jwt = njwt.create(claims, readSecreteKeyFile(), 'HS256');
+    jwt.setExpiration(expiration_date);
+
+    return {
+        access_token: jwt.compact(),
+        token_type: 'Bearer',
+        expires_in: expiration_date.getTime() - Date.now()
+    };
+}
+
+/**
+ * Read secret key file
+ * @returns {*}
+ */
 function readSecreteKeyFile()
 {
         return fs.readFileSync(process.cwd() + '/server/Keys/imok.pem');
 }
 
-module.exports = authorize;
+module.exports = {
+    authorize: authorize,
+    generateToken: generateToken
+};
