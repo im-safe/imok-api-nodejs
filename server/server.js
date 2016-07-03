90e9e99c0246c7e16f817a6e7afa0ed8517cc9e0
Saleh Saeed <saleh.saiid@gmail.com>
2016-07-03 03:59:05 +0300

    Enable CROSS

--- server/server.js
+++ server/server.js
@@ -36,6 +36,19 @@ app.use(function(req, res, next){
         'Access-Control-Allow-Headers',
         'X-Requested-With,content-type, Authorization'
     );
+
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        '*'
+    );
+
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    );
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     next();
 });
 app.use(session({
